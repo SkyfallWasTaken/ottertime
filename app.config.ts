072ac12738edafa0a1b065dac99d1c6976effbf9
Mahad Kalam <mahadkalam1@proton.me>
2025-03-23 23:09:11 +0000

    Add CF Pages support

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,24 @@
-import { defineConfig } from '@tanstack/react-start/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@tanstack/react-start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { cloudflare } from "unenv";
+
+const isPages = !!process.env.CF_PAGES;
 
 export default defineConfig({
-  tsr: {
-    appDirectory: 'src',
-  },
-  vite: {
-    plugins: [
-      tsConfigPaths({
-        projects: ['./tsconfig.json'],
-      }),
-    ],
-  },
-})
+	tsr: {
+		appDirectory: "src",
+	},
+	vite: {
+		plugins: [
+			tsConfigPaths({
+				projects: ["./tsconfig.json"],
+			}),
+		],
+	},
+	server: isPages
+		? {
+				preset: "cloudflare-pages",
+				unenv: cloudflare,
+			}
+		: undefined,
+});
