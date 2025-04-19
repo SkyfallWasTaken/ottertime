31bafbcbcc2f1bc1e9736daf5df9075810e7df16
Mahad Kalam <55807755+SkyfallWasTaken@users.noreply.github.com>
2025-04-19 22:34:04 +0000

    Remove unused class

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -39,7 +39,7 @@ function Home() {
         <p className="text-muted-foreground text-lg italic">{quote}</p>
       </div>
 
-      <div className="flex gap-4 w-full grid grid-cols-2 md:grid-cols-5">
+      <div className="gap-4 w-full grid grid-cols-2 md:grid-cols-5">
         <StatCard title="Total Time" value="46h 25m" />
         <StatCard title="Top Project" value="Quackatime" />
         <StatCard title="Top Language" value="Rust" className='bg-orange-600/80' />
