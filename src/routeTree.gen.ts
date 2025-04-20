1153c0e60c1b09847ff0ed09eb5db941e8ba19d9
Mahad Kalam <55807755+SkyfallWasTaken@users.noreply.github.com>
2025-04-20 10:49:46 +0000

    Add setup page

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -12,6 +12,7 @@
 
 import { Route as rootRoute } from './routes/__root'
 import { Route as StatisticsImport } from './routes/statistics'
+import { Route as SetupImport } from './routes/setup'
 import { Route as SettingsImport } from './routes/settings'
 import { Route as IndexImport } from './routes/index'
 
@@ -20,6 +21,12 @@ import { Route as IndexImport } from './routes/index'
 const StatisticsRoute = StatisticsImport.update({
   id: '/statistics',
   path: '/statistics',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const SetupRoute = SetupImport.update({
+  id: '/setup',
+  path: '/setup',
   getParentRoute: () => rootRoute,
 } as any)
 
@@ -53,6 +60,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof SettingsImport
       parentRoute: typeof rootRoute
     }
+    '/setup': {
+      id: '/setup'
+      path: '/setup'
+      fullPath: '/setup'
+      preLoaderRoute: typeof SetupImport
+      parentRoute: typeof rootRoute
+    }
     '/statistics': {
       id: '/statistics'
       path: '/statistics'
@@ -68,12 +82,14 @@ declare module '@tanstack/react-router' {
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
   '/settings': typeof SettingsRoute
+  '/setup': typeof SetupRoute
   '/statistics': typeof StatisticsRoute
 }
 
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
   '/settings': typeof SettingsRoute
+  '/setup': typeof SetupRoute
   '/statistics': typeof StatisticsRoute
 }
 
@@ -81,27 +97,30 @@ export interface FileRoutesById {
   __root__: typeof rootRoute
   '/': typeof IndexRoute
   '/settings': typeof SettingsRoute
+  '/setup': typeof SetupRoute
   '/statistics': typeof StatisticsRoute
 }
 
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
-  fullPaths: '/' | '/settings' | '/statistics'
+  fullPaths: '/' | '/settings' | '/setup' | '/statistics'
   fileRoutesByTo: FileRoutesByTo
-  to: '/' | '/settings' | '/statistics'
-  id: '__root__' | '/' | '/settings' | '/statistics'
+  to: '/' | '/settings' | '/setup' | '/statistics'
+  id: '__root__' | '/' | '/settings' | '/setup' | '/statistics'
   fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
   SettingsRoute: typeof SettingsRoute
+  SetupRoute: typeof SetupRoute
   StatisticsRoute: typeof StatisticsRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   SettingsRoute: SettingsRoute,
+  SetupRoute: SetupRoute,
   StatisticsRoute: StatisticsRoute,
 }
 
@@ -117,6 +136,7 @@ export const routeTree = rootRoute
       "children": [
         "/",
         "/settings",
+        "/setup",
         "/statistics"
       ]
     },
@@ -125,6 +145,9 @@ export const routeTree = rootRoute
     },
     "/settings": {
       "filePath": "settings.tsx"
+    },
+    "/setup": {
+      "filePath": "setup.tsx"
     },
     "/statistics": {
       "filePath": "statistics.tsx"
