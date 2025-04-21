0dd49684db29262d1b784b06aa24b498851187d1
SkyfallWasTaken <mahadkalam1@proton.me>
2025-04-21 13:56:19 +0000

    Add some debug logging

--- src/routes/api/auth/$.ts
+++ src/routes/api/auth/$.ts
@@ -3,9 +3,11 @@ import { createAPIFileRoute } from "@tanstack/react-start/api";
 
 export const APIRoute = createAPIFileRoute("/api/auth/$")({
   GET: ({ request }) => {
+    console.log(request.url + " GET");
     return auth.handler(request);
   },
   POST: ({ request }) => {
+    console.log(request.url + " POST");
     return auth.handler(request);
   },
 });
