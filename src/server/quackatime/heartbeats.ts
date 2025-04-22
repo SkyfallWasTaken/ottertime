53a43f3faa4a28e2b56f27657f916b40229f61f4
SkyfallWasTaken <mahadkalam1@proton.me>
2025-04-22 21:24:49 +0000

    Switch heartbeat hashes to a stable JSON.stringify

--- src/server/quackatime/heartbeats.ts
+++ src/server/quackatime/heartbeats.ts
@@ -1,5 +1,6 @@
 import { db, heartbeats as heartbeatsTable } from "../db";
 import type { Heartbeat } from "~/common/heartbeats";
+import stableJsonStringify from "fast-json-stable-stringify";
 
 export default async function emitHeartbeats(
   heartbeats: Heartbeat[],
@@ -16,24 +17,10 @@ export default async function emitHeartbeats(
           };
           return {
             ...rawHb,
-            hash: await hashHeartbeat(rawHb),
+            hash: stableJsonStringify(rawHb),
           };
         })
       )
     )
     .onConflictDoNothing();
-}
-
-export function hashHeartbeat(
-  heartbeat: Omit<typeof heartbeatsTable.$inferInsert, "hash">
-) {
-  // We do this for a stable hash
-  const heartbeatString = `userId: ${heartbeat.userId}, entity: ${heartbeat.entity}, type: ${heartbeat.type}, time: ${heartbeat.time}, lineno: ${heartbeat.lineNo}, cursorPos: ${heartbeat.cursorPos}, isWrite: ${heartbeat.isWrite}, project: ${heartbeat.project}, branch: ${heartbeat.branch}, language: ${heartbeat.language}, dependencies: ${heartbeat.dependencies}, lines: ${heartbeat.lines}, lineAdditions: ${heartbeat.lineAdditions}, lineDeletions: ${heartbeat.lineDeletions}, projectRootCount: ${heartbeat.projectRootCount},`;
-  return crypto.subtle
-    .digest("SHA-256", new TextEncoder().encode(heartbeatString))
-    .then((hashBuffer) => {
-      return Array.from(new Uint8Array(hashBuffer))
-        .map((b) => b.toString(16).padStart(2, "0"))
-        .join("");
-    });
 }
