0b2e9c8c7aec788c8fc16130bf699ec3e5100df5
SkyfallWasTaken <mahadkalam1@proton.me>
2025-04-28 17:36:18 +0000

    Fix SHA256 function to work on the server too

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -57,7 +57,7 @@ export function getPasswordFromAuthHeader(authHeader: string):
 
 export async function sha256(message: string) {
   const msgUint8 = new TextEncoder().encode(message);
-  const hashBuffer = await window.crypto.subtle.digest("SHA-256", msgUint8);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
   const hashHex = hashArray
     .map((b) => b.toString(16).padStart(2, "0"))
