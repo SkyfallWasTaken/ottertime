97da779ebd40852ebcce07b17ccc79dbb22451e2
SkyfallWasTaken <mahadkalam1@proton.me>
2025-04-28 19:49:05 +0000

    Refactor environment variable import path to use utils

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -10,7 +10,7 @@ import type { Context } from "./util";
 
 // Run the checks to ensure the env variables are set
 // If they aren't, the API won't work and this will be detected by Coolify health checks
-import "~/env";
+import "~/utils/env";
 
 const app = new Hono<Context>();
 
