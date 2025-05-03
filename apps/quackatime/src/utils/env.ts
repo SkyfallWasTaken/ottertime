b1a705179c1a237a005a92c35bece62db005586c
SkyfallWasTaken <mahadkalam1@proton.me>
2025-05-03 09:57:06 +0000

    Update Turbo config w/ new env vars

--- apps/quackatime/src/utils/env.ts
+++ apps/quackatime/src/utils/env.ts
@@ -1,6 +1,7 @@
 import { createEnv } from "@t3-oss/env-core";
 import { z } from "zod";
 
+// Make sure to update the Turbo config to include new env variables!
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
