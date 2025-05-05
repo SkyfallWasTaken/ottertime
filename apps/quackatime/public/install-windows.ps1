db6f0c94ae294ded67f4d944084e92c5271530ba
SkyfallWasTaken <mahadkalam1@proton.me>
2025-05-05 11:44:05 +0000

    Bye byte TS Start

--- apps/quackatime/public/install-windows.ps1
+++
@@ -1,3 +0,0 @@
-# By default, we use the official Quackatime installer.
-# If you want to change this and use your own, change the `public/install-windows.ps1` file.
-irm https://raw.githubusercontent.com/quackatime/extension-installer/refs/heads/main/install_wakatime.ps1 | iex
