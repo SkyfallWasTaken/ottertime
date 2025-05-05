23c4c0df77c8cd35ab99fd8745463890de511087
SkyfallWasTaken <mahadkalam1@proton.me>
2025-05-05 13:49:26 +0000

    Re-add installation scripts

---
+++ apps/web/public/install-unix.sh
@@ -0,0 +1,3 @@
+# By default, we use the official OtterTime installer.
+# If you want to change this and use your own, change the `public/install-unix.sh` file.
+curl -fsSL https://raw.githubusercontent.com/ottertime/extension-installer/refs/heads/main/install_wakatime.sh | bash
---
+++ apps/web/public/install-windows.ps1
@@ -0,0 +1,3 @@
+# By default, we use the official OtterTime installer.
+# If you want to change this and use your own, change the `public/install-windows.ps1` file.
+irm https://raw.githubusercontent.com/ottertime/extension-installer/refs/heads/main/install_wakatime.ps1 | iex
