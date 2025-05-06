134ca078ce6ba4d8a1c25e5618549551b2c3db89
SkyfallWasTaken <mahadkalam1@proton.me>
2025-05-06 09:54:42 +0000

    Fix captcha not working on invalid signin

--- apps/web/app/components/turnstile.tsx
+++ apps/web/app/components/turnstile.tsx
@@ -1,19 +1,26 @@
-import { Turnstile as TurnstileWidget } from "@marsidev/react-turnstile";
+import {
+	Turnstile as TurnstileWidget,
+	type TurnstileInstance,
+} from "@marsidev/react-turnstile";
 import { useTheme } from "remix-themes";
 import { env } from "@repo/env/client";
+import { useRef, type Ref } from "react";
 
 export default function Turnstile({
 	onSuccess,
 }: { onSuccess: (token: string) => void }) {
 	const [theme] = useTheme();
+	const ref: Ref<TurnstileInstance | null> = useRef(null);
 	return (
 		<div className="border-r border-[#797979] border-[1px]">
 			<TurnstileWidget
 				siteKey={env.VITE_TURNSTILE_SITE_KEY}
+				ref={ref}
 				options={{
 					theme: theme || "auto",
 					size: "flexible",
 				}}
+				onError={() => ref.current?.reset()}
 				onSuccess={(token) => {
 					onSuccess(token);
 				}}
